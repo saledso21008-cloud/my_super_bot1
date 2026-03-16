e55b01ef9a7232dd50366c3559af3ad58a823528
saledso21008-cloud <saledso21008@gmail.com>
2026-03-16 17:38:25 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -127,11 +127,12 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_text("✅ Отправлено")
         return
 
-        if text == "📊 Получить Excel" and is_admin(user.id):
+    if text == "📊 Получить Excel" and is_admin(user.id):
         data = db_cursor.execute("""
             SELECT 
                 u.user_id,
                 u.full_name,
+                h.subject,
                 h.duration_seconds,
                 h.start_time,
                 h.end_time,
@@ -147,13 +148,13 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
         rows = []
         for row in data:
-            user_id, name, sec, start, end, date = row
+            user_id, name, subject, sec, start, end, date = row
             minutes = sec // 60
             seconds = sec % 60
             time_str = f"{minutes} мин {seconds} сек" if minutes > 0 else f"{seconds} сек"
-            rows.append([user_id, name, time_str, f"{start} — {end}", date])
+            rows.append([user_id, name, subject, time_str, f"{start} — {end}", date])
 
-        df = pd.DataFrame(rows, columns=["ID", "Имя", "Время", "Начало — Конец", "Дата"])
+        df = pd.DataFrame(rows, columns=["ID", "Имя", "Предмет", "Время", "Начало — Конец", "Дата"])
         df.to_excel("data.xlsx", index=False)
 
         with open("data.xlsx", "rb") as f:
